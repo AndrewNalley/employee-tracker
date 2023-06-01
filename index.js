ecc36fd4b7dd74f921c14e84c7980aac865d5488
AndrewNalley <104536377+AndrewNalley@users.noreply.github.com>
2023-05-31 22:12:59 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const inquirer = require('inquirer');
+const connection = require('./db/connection')
+
+function init() {
+inquirer prompt([
+    {
+    message: "What would you like to do?",
+    name: "choice",
+    type: "list",
+    choices: ["View all departments", "View all roles", "View all roles", "Quit"]
+    }
+]).then(answers => {
+    switch(answers) {
+        case "view all departments":
+            break;
+        case "View all roles":
+            viewAllRoles();
+            break;
+        case "quit":
+            console.log("Goodbye!")
+            process.exit(0)     
+    }
+})
+}
+function viewAllRoles() {
+    connection.promise().query("SELECT * FROM role")
+    .then(data => {
+        console.table(data)
+    })
+    .then(() => init())
+}
+
+init()
