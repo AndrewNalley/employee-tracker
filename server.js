0cd696c30fcd5033027707b8b3c4b77de0a7d2e1
AndrewNalley <104536377+AndrewNalley@users.noreply.github.com>
2023-06-20 19:27:08 -0500

    add post, put, and delete routes. Need to seed database

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ const db = mysql.createConnection({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+// get all employees route
 app.get(`/api/employee`, (req, res) => {
-    db.query(`SELECT * FROM employee`, (err, data) => {
+    db.query(`SELECT * FROM employee`, (err, result) => {
         if (err) {
             res.status(500).json({
                 success: false,
@@ -24,8 +24,72 @@ app.get(`/api/employee`, (req, res) => {
         }
         res.json({
             success: true,
-            body: data
+            body: result
         });
     })
 });
-    app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+// add new employee route
+app.post(`/api/employee`, (req, res) => {
+    const { first_name, last_name, role_id } = req.body;
+    db.query(`INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?, ?)`,
+        [first_name, last_name, role_id],
+        (err, result) => {
+            if (err) {
+                res.status(500).json({
+                    success: false,
+                    error: err
+                })
+            }
+            res.status(201).json({
+                success: true,
+                body: req.body
+            });
+        })
+});
+// update employee route
+app.put("/api/employee/:id", (req, res) => {
+    db.query(`UPDATE employee SET first_name =?, last_name =?, role_id =? WHERE id =?`, [req.body.first_name, req.body.last_name, req.body], (err, result) => {
+        if (err) {
+            res.status(400).json({
+                success: false,
+                error: err
+            })
+        } else if (!result.affectedRows) {
+            res.status(404).json({
+                success: false,
+                error: "Employee not found"
+            })
+        } else {
+            res.json({
+                success: true,
+                data: req.body,
+                changes: result.affectedRows
+            })
+        }
+    })
+});
+
+app.delete("/api/employee/:id", (req, res) => {
+    db.query(`DELETE FROM employee WHERE id =?`, [req.params.id], (err, result) => {
+        if (err) {
+            res.status(400).json({
+                success: false,
+                error: err
+            })
+        } else if (!result.affectedRows) {
+            res.status(404).json({
+                success: false,
+                error: "Employee not found"
+            })
+        } else {
+            res.json({
+                success: true,
+                message: "Employee deleted",
+                changes: result.affectedRows
+            })
+        }
+    });
+});
+
+// server listening
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
