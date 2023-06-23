9ac565cd75d99f3bf9f4502cb7c13683cd93769c
AndrewNalley <104536377+AndrewNalley@users.noreply.github.com>
2023-06-23 13:36:01 -0500

    add seeds file

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const inquirer = require('inquirer');
-const connection = require('./server')
+const db = require('./server')
 
 function inquirerInit() {
     inquirer
@@ -24,11 +24,93 @@ function inquirerInit() {
         })
 }
 function viewAllRoles() {
-    connection.promise().query("SELECT * FROM role")
+    db.promise().query("SELECT * FROM role")
         .then(data => {
             console.table(data)
         })
         .then(() => init())
 }
+
+
+
+db.query(`SELECT * FROM employee`, (err, result) => {
+    if (err) {
+        res.status(500).json({
+            success: false,
+            error: err
+        })
+    }
+    res.json({
+        success: true,
+        body: result
+    });
+})
+
+
+// add new employee 
+
+const { first_name, last_name, role_id } = req.body;
+db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`,
+    [first_name, last_name, role_id, manager_id],
+    (err, result) => {
+        if (err) {
+            res.status(500).json({
+                success: false,
+                error: err
+            })
+        }
+        res.status(201).json({
+            success: true,
+            body: req.body
+        });
+    })
+});
+
+// update employee 
+
+db.query(`UPDATE employee SET first_name =?, last_name =?, role_id =? WHERE id =?`, [req.body.first_name, req.body.last_name, req.body], (err, result) => {
+    if (err) {
+        res.status(400).json({
+            success: false,
+            error: err
+        })
+    } else if (!result.affectedRows) {
+        res.status(404).json({
+            success: false,
+            error: "Employee not found"
+        })
+    } else {
+        res.json({
+            success: true,
+            data: req.body,
+            changes: result.affectedRows
+        })
+    }
+});
+
+
+// delete employee 
+
+db.query(`DELETE FROM employee WHERE id =?`, [req.params.id], (err, result) => {
+    if (err) {
+        res.status(400).json({
+            success: false,
+            error: err
+        })
+    } else if (!result.affectedRows) {
+        res.status(404).json({
+            success: false,
+            error: "Employee not found"
+        })
+    } else {
+        res.json({
+            success: true,
+            message: "Employee deleted",
+            changes: result.affectedRows
+        })
+    }
+});
+
+
 
 inquirerInit();
